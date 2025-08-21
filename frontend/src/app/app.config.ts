3d0461af85141a637b2ea30f762c649ff032bee9
𝕃𝕦𝕔𝕒𝕤 <lucas.skyzoc@gmail.com>
2025-08-21 15:12:55 -0300

    add auth interceptor for JWT token in requests

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 // NASA STANDARD: Configuration < 60 lines, fail-fast setup
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
